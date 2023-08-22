26d79e99cd037d193f6904ffe024785402e9bc6f
RangiraDave <rangiradev666@gmail.com>
2023-08-22 23:50:39 +0200

    adding old logic

---
+++ handle_%.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * handle_mod - Function to handle %
+ * @specifier: The charactor to be checked
+ * Return: 0 or 1
+ */
+int handle_mod(char specifier)
+{
+	if (specifier == '%')
+	{
+		write(1, "%", 1);
+		return (1);
+	}
+	return (0);
+}
---
+++ handle_int.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <stdarg.h>
+/**
+ * handle_int - Function to handle all integers.
+ * @passed_arg: Passed character
+ * @specifier: The character to be checked
+ * Return: 1 or 0
+ */
+
+int handle_int(va_list passed_arg, char specifier)
+{
+	int num, i, j;
+	char temp, str[20];
+
+	if (specifier != 'd')
+		return (0);
+	num = va_arg(passed_arg, int);
+	if (num == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	if (num < 0)
+	{
+		write(1, "-", 1);
+		num = -num;
+	}
+	i = 0;
+	while (num)
+	{
+		str[i++] = num % 10 + '0';
+		num /= 10;
+	}
+
+	for (j = 0; j < i / 2; j++)
+	{
+		temp = str[j];
+		str[j] = str[i - j - 1];
+		str[i - j - 1] = temp;
+	}
+	write(1, str, sizeof(char) * i);
+	return (i);
+}
